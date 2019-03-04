0cf92e6dd02b94d02d8c78c2ca58e5b82f61a037
Mahibub Shaikh <mahibub@gmail.com>
2019-03-04 17:41:59 +0100

    Modified shell script

--- run_all.sh
+++ run_all.sh
@@ -1,20 +1,28 @@
-if [ -d "test_results" ]; then
-    echo "folder test_results exist"
-else
-    mkdir test_results
-fi
-echo '-Building Application-'
-catkin_make install
-echo "-Doxygen Results-"
+#!/bin/sh
+#. /opt/ros/kinetic/setup.sh
+mkdir test_results
+mkdir test_results/coverage
+mkdir test_results/gtest
+mkdir test_results/cppcheck
+mkdir test_results/cpplint
+echo 'Building Application'
+#catkin_make install
+echo 'Executing UTs'
+#catkin_make run_tests >> test_results/gtest/Unit_testResult.txt
+echo 'Code Coverage'
+gcovr -r . -e ".*\.h" --exclude=".*test.cc" --exclude=".*_node.cc" --html -o test_results/coverage/coverage.html --html-details
+gcovr -r . --exclude=".*test.cc" --exclude=".*_node.cc" --xml-pretty > test_results/coverage/cov.xml
+gcovr -r . --exclude=".*test.cc" --exclude=".*_node.cc" --xml > test_results/coverage/cov_small.xml
+echo "Doxygen Results"
 doxygen dox_config.conf
-echo '-Guidelines Report-'
+echo 'Guidelines Report'
 cd src
-cpplint *.cc >>../test_results/Guidelines_Report.txt
-echo '-Guidelines Summary-'
-cpplint --counting=detailed     $( find . -name \*.h -or -name \*.cc | grep -vE "^\.\/build\/" ) 2>&1 |     grep -e "Category" -e "Total error" >>../test_results/Guidelines_summary.txt
+cpplint --counting=detailed     $( find . -name \*.h -or -name \*.cc | grep -vE "^\.\/build\/" ) >> ../test_results/cpplint/Guidelines_detail.txt 2>&1
+cpplint --counting=detailed     $( find . -name \*.h -or -name \*.cc | grep -vE "^\.\/build\/" ) 2>&1 |     grep -e "Category" -e "Total error" >> ../test_results/cpplint/Guidelines_summary.txt
+echo 'cppcheck'
 cd ..
-
-
-
-
-
+cppcheck --xml --xml-version=2 --enable=all . 2> test_results/cppcheck/cppcheck.xml
+echo 'Copying gtest XMLs'
+cp build/test_results/ros_arithmetic/gtest-ros_arithmetic_test.xml test_results/gtest/gtest-ros_arithmetic_test.xml
+cp build/test_results/ros_number_generator/gtest-ros_number_generator_test.xml test_results/gtest/gtest-ros_number_generator_test.xml
+echo 'DONE'
